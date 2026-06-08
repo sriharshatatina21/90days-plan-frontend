a6e1984aaa045215aa3ff8c6975587ae06c33921
SriHarsha <sriharshatatina21@gmail.com>
2026-06-08 09:40:51 +0530

    changed

---
+++ functions.js
@@ -0,0 +1,16 @@
+//function is a block of code that can use number of times means same logic with no of times print
+// constructor
+// function person(name,age){
+//     this.name = name;
+//     this.age = age; --> this is used to for refer to present element in an object or function where if we 
+// want to not to show the varibale directly we use this
+// }
+// let person1 = new person("sai",23)//--> this is a constructor where we create a obj to person
+// console.log(person1);
+
+
+
+
+
+
+
